64fa00b0dbe4c9d17c0e8a9b385d5a1cf7a8ce97
Aditya Pawar <adityapawar444@gmail.com>
2025-11-27 22:29:57 +0530

    basic llm-app with Google Google Gen AI API

---
+++ llm-app/README.md
@@ -0,0 +1,3 @@
+Create Virtual Env with all dependencies - uv sync
+Actiavte Virtual Enc - source .venv/bin/activate
+Run the App - python -m streamlit run app.py
---
+++ llm-app/app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from langchain_google_genai import GoogleGenerativeAI
+
+# api_key = "AIzaSyA6VfZn5ilN3MeBzUgMh0DZAt6HiNb5MY4"
+st.title("Simple LLM App")
+
+
+def generate_response(input):
+    llm = GoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        google_api_key=api_key
+    )
+    st.info(llm.invoke(input))
+
+
+with st.form('my_form'):
+
+    if "api_key" not in st.session_state:
+        api_key = st.text_input(
+            "Enter your API key:",
+            type="password"
+        )
+        if api_key:
+            st.session_state.api_key = api_key
+            st.success("API Key saved.")
+        else:
+            st.warning('Please Enter your API Key', icon='⚠')
+    text = st.text_area('Enter Text:', '')
+    submitted = st.form_submit_button('Submit')
+    if submitted:
+        generate_response(text)
